1f8c74a69462eb08590ff4255e84554ad41df7de
wildanfh <wildanf2005@gmail.com>
2025-08-28 19:33:32 +0200

    fix tailwind postcss setup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: {
-      plugins: [tailwindcss],
-    },
+    postcss: './postcss.config.js', // pastikan ada file ini
   },
 });
+
